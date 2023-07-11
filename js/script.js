d7afb606ec104a2a91ee5d96bd754bbb422b9d30
Qashqiriston <akobirtojiyev3@gmail.com>
2023-07-11 17:04:47 +0500

    full app.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const canvas = document.querySelector('canvas'),
   toolBtns = document.querySelectorAll('.tool'),
-  fillColor = document.querySelector('#fill-color')
+  fillColor = document.querySelector('#fill-color'),
+  sizeSlider = document.querySelector('#size-slider'),
+  colorBtns = document.querySelectorAll('.colors .option'),
+  colorPicker = document.querySelector('#color-picker'),
+  clearCanvas = document.querySelector('.clear-canvas'),
+  saveImg  = document.querySelector('.save-img')
+
 
 let ctx = canvas.getContext('2d'),
    isDrawing = false,
    brushWith = 3,
    selectedTool = 'brush',
+   selectedColor = "#000",
    prevMouseX,
    prevMouseY,
    snapshot
 
+const setConvasBackraund = () =>{
+  ctx.fillStyle = '#fff'
+  ctx.fillRect(0 , 0 , canvas.width, canvas.height)
+  ctx.fillStyle = selectedColor
+}
+
 window.addEventListener('load', ()=>{
   canvas.width = canvas.offsetWidth
   canvas.height = canvas.offsetHeight
+  setConvasBackraund()
 })
 const startDrow = e =>{
   isDrawing = true
@@ -20,6 +34,8 @@ const startDrow = e =>{
   prevMouseY = e.offsetY
   ctx.beginPath()
   ctx.lineWidth = brushWith
+  ctx.strokeStyle = selectedColor
+  ctx.fillStyle = selectedColor
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
 }
 
@@ -29,6 +45,15 @@ drawRectangle = e =>{
   }else{
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY )
   }
+}
+
+drawTriangle = e => {
+  ctx.beginPath()
+  ctx.moveTo(prevMouseX, prevMouseY)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY)
+  ctx.closePath()
+  fillColor.checked ? ctx.fill() :  ctx.stroke()
 }
 
 const drawCircle = e => {
@@ -51,6 +76,15 @@ const drawing = e =>{
       break
       case 'circle':
         drawCircle(e)
+      break
+      case "triangle":
+        drawTriangle(e)
+      break  
+      case 'eraser':
+        ctx.strokeStyle = '#fff'
+        ctx.lineTo(e.offsetX, e.offsetY)
+        ctx.stroke()
+        break
       default: break
     }
   }
@@ -65,6 +99,35 @@ toolBtns.forEach(btn=>{
     selectedTool = btn.id
     console.log(selectedTool);
   })
+})
+
+sizeSlider.addEventListener('change', () => brushWith = sizeSlider.value)
+
+colorBtns.forEach(btn => {
+  btn.addEventListener('click', e =>{ 
+  document.querySelector('.options .selected').classList.remove('selected')
+  btn.classList.add('selected')
+  const bgColor = window.getComputedStyle(btn).getPropertyValue('background-color')
+  selectedColor = bgColor
+    console.log(selectedColor);
+  })
+})
+
+colorPicker.addEventListener('change', ()=>{
+  colorPicker.parentElement.style.background = colorPicker.value
+  colorPicker.parentElement.click()
+})
+
+clearCanvas.addEventListener('click', ()=>{
+  ctx.clearRect(0 , 0 ,canvas.width, canvas.height)
+  setConvasBackraund()
+})
+
+saveImg.addEventListener('click', () =>{
+  const link = document.createElement('a')
+  link.download = `Akobir-points${Date.now()}.jpg`
+  link.href = canvas.toDataURL()
+  link.click()
 })
 
 canvas.addEventListener('mousedown', startDrow)
